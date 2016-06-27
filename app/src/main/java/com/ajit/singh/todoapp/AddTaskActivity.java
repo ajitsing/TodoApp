3e80f6ef1f9d93fe9562ae283da04cbedea7d31e
AjitSingh <jeetsingh.ajit@gmail.com>
2016-06-27 07:43:50 +0530

    add task activity with binding

--- app/src/main/java/com/ajit/singh/todoapp/AddTaskActivity.java
+++ app/src/main/java/com/ajit/singh/todoapp/AddTaskActivity.java
@@ -1,14 +1,14 @@
 package com.ajit.singh.todoapp;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 import com.ajit.singh.todoapp.actions.AddTaskActions;
 import com.ajit.singh.todoapp.database.repository.TaskRepository;
+import com.ajit.singh.todoapp.databinding.ActivityAddTaskBinding;
 import com.ajit.singh.todoapp.presenter.AddTaskPresenter;
+import com.ajit.singh.todoapp.viewmodel.AddTaskViewModel;
 
 @SuppressWarnings("ALL")
 public class AddTaskActivity extends AppCompatActivity implements AddTaskActions {
@@ -18,32 +18,17 @@ public class AddTaskActivity extends AppCompatActivity implements AddTaskActions
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_add_task);
 
-    presenter = new AddTaskPresenter(new TaskRepository(this), this);
+    ActivityAddTaskBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_add_task);
+    final AddTaskViewModel viewModel = new AddTaskViewModel();
+    binding.setViewModel(viewModel);
 
-    Button doneButton = (Button) findViewById(R.id.add_task);
-
-    doneButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        addNewTask();
-      }
-    });
+    AddTaskPresenter presenter = new AddTaskPresenter(new TaskRepository(this), this);
+    binding.setPresenter(presenter);
   }
 
   @Override
   public void navigateBack() {
     finish();
-  }
-
-  private void addNewTask() {
-    EditText titleEditText = (EditText) findViewById(R.id.task_title);
-    EditText descriptionEditText = (EditText) findViewById(R.id.task_description);
-
-    String title = titleEditText.getText().toString();
-    String description = descriptionEditText.getText().toString();
-
-    presenter.onAddTask(title, description);
   }
 }
